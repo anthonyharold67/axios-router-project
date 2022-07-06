c1a14f33f9cf0234399c7143a20820af8c8d5c35
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-07-06 18:01:21 +0300

    completed

--- src/components/CardFollowers.jsx
+++ src/components/CardFollowers.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Col,Card,Button} from "react-bootstrap"
 
-const CardFollowers = () => {
+const CardFollowers = ({follower}) => {
   return (
-    <div>CardFollowers</div>
+    <div>
+      <Col>
+        <Card>
+        <Card.Img variant="top" src={follower.avatar_url} />
+        <Card.Body>
+        <Card.Text>{follower.login}</Card.Text>
+        <Button
+        href={follower.html_url}
+        style={{borderRadius:'50px'}}
+        variant="primary">VIEW PROFILE</Button>
+        </Card.Body>
+        </Card>
+      </Col>
+    </div>
   )
 }
 
