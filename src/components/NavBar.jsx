dad185699ed91a43110923125c653e24fab48643
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-07-06 16:40:04 +0300

    navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,26 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from "react-router-dom"
 const NavBar = () => {
+  const [isOpen,setIsOpen]=React.useState(sessionStorage.getItem("email") || false)
+
+  const handleClick=()=>{
+    setIsOpen(false)
+    sessionStorage.clear()
+  }
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        {isOpen ? (<Container>
+          <Navbar.Brand href="/home">Clarusway</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/" onClick={handleClick}>Logout</NavLink>
           </Nav>
-        </Container>
+        </Container>):(<Container>
+          <Navbar.Brand href="">Clarusway</Navbar.Brand></Container>)}
       </Navbar>
     </div>
   )
