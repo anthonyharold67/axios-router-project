c8d1ffe0e73f8c264cc7be38422d9221b3284357
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-07-06 17:55:22 +0300

    pagination added

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Pagination from 'react-bootstrap/Pagination';
 
-const Paginate = () => {
+const Paginate = ({pages,setCurrentPage}) => {
+  const [activePage,setActivePage] = React.useState(1)
+  
+let items = [];
+for (let number = 1; number <= pages; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === activePage} onClick={()=>setActivePage(number)}>
+      {number}
+    </Pagination.Item>,
+  );
+}
+ React.useEffect(()=>{
+  setCurrentPage(activePage)
+ },[activePage,setCurrentPage])
   return (
-    <div>Paginate</div>
+    <div>
+      <Pagination>{items}</Pagination>
+    </div>
   )
 }
 
